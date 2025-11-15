83dbd06926656ccf97e02bb1beff81b6ebbd4b00
lizhenyu <2945659544@qq.com>
2025-11-15 13:45:47 +0800

    根据不同等级划分文件夹

--- ZlogFactory.cs
+++ ZlogFactory.cs
@@ -26,40 +26,42 @@ public static class ZlogFactory
 
     private static ILoggerFactory CreateDefaultFactory()
     {
-        return LoggerFactory.Create(logging =>
+        // 创建 Trace/Debug 日志工厂
+        var traceFactory = LoggerFactory.Create(logging =>
         {
-            logging.ClearProviders();
             logging.SetMinimumLevel(LogLevel.Trace);
-            // Add to output to console // logging.AddZLoggerConsole();
+            logging.AddZLoggerRollingFile(options =>
+            {
+                options.FilePathSelector = (timestamp, sequenceNumber) =>
+                    $"logs/trace/{timestamp.ToLocalTime():yyyy-MM-dd-HH}_{sequenceNumber:000}.log";
+                options.RollingInterval = RollingInterval.Hour;
+                options.RollingSizeKB = 1024 * 2;
 
-            //trace 和 debug
-            logging
-                 .AddFilter((_, __, lvl) => lvl is LogLevel.Trace or LogLevel.Debug)
-                .AddZLoggerRollingFile(options =>
-                {
-                    options.FilePathSelector = (timestamp, sequenceNumber) =>
-                        $"logs/trace/{timestamp.ToLocalTime():yyyy-MM-dd-HH}_{sequenceNumber:000}.log";
-                    options.RollingInterval = RollingInterval.Hour;
-                    options.RollingSizeKB = 1024 * 2;
-
-                    ConfigureFormatter(options);
-                    ConfigureOptions(options);
-                });
-
-            // Info及以上 文件
-            logging
-               .AddFilter((_, __, lvl) => lvl >= LogLevel.Information)
-               .AddZLoggerRollingFile(options =>
-               {
-                   options.FilePathSelector = (timestamp, sequenceNumber) =>
-                       $"logs/{timestamp.ToLocalTime():yyyy-MM-dd-HH}_{sequenceNumber:000}.log";
-                   options.RollingInterval = RollingInterval.Hour;
-                   options.RollingSizeKB = 1024 * 2;
-
-                   ConfigureFormatter(options);
-                   ConfigureOptions(options);
-               });
+                ConfigureFormatter(options);
+                ConfigureOptions(options);
+            });
+            // 只记录 Trace 和 Debug
+            logging.AddFilter((category, level) => level < LogLevel.Information);
         });
+
+        // 创建 Info 及以上日志工厂
+        var infoFactory = LoggerFactory.Create(logging =>
+        {
+            logging.SetMinimumLevel(LogLevel.Information);
+            logging.AddZLoggerRollingFile(options =>
+            {
+                options.FilePathSelector = (timestamp, sequenceNumber) =>
+                    $"logs/{timestamp.ToLocalTime():yyyy-MM-dd-HH}_{sequenceNumber:000}.log";
+                options.RollingInterval = RollingInterval.Hour;
+                options.RollingSizeKB = 1024 * 2;
+
+                ConfigureFormatter(options);
+                ConfigureOptions(options);
+            });
+        });
+
+        // 返回组合工厂
+        return new CompositeLoggerFactory(traceFactory, infoFactory);
     }
 
     private static void ConfigureFormatter(ZLoggerRollingFileOptions options)
@@ -99,4 +101,73 @@ public static class ZlogFactory
     }
 
     public static ILogger<T> Get<T>() => Factory.CreateLogger<T>();
+}
+
+// 组合多个 LoggerFactory
+internal class CompositeLoggerFactory(params ILoggerFactory[] factories) : ILoggerFactory
+{
+    private readonly ILoggerFactory[] _factories = factories;
+
+    public void AddProvider(ILoggerProvider provider)
+    {
+        foreach (var factory in _factories) factory.AddProvider(provider);
+    }
+
+    public ILogger CreateLogger(string categoryName)
+    {
+        var loggers = _factories.Select(f => f.CreateLogger(categoryName)).ToArray();
+        return new CompositeLogger(loggers);
+    }
+
+    public void Dispose()
+    {
+        foreach (var factory in _factories) factory.Dispose();
+    }
+}
+
+// 组合多个 Logger，同时写入
+internal class CompositeLogger(params ILogger[] loggers) : ILogger
+{
+    private readonly ILogger[] _loggers = loggers;
+
+    public IDisposable? BeginScope<TState>(TState state) where TState : notnull
+    {
+        var scopes = new IDisposable?[_loggers.Length];
+        for (int i = 0; i < _loggers.Length; i++)
+        {
+            scopes[i] = _loggers[i].BeginScope(state);
+        }
+        return new CompositeDisposable(scopes);
+    }
+
+    public bool IsEnabled(LogLevel logLevel)
+    {
+        for (int i = 0; i < _loggers.Length; i++)
+        {
+            if (_loggers[i].IsEnabled(logLevel)) return true;
+        }
+        return false;
+    }
+
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception,
+        Func<TState, Exception?, string> formatter)
+    {
+        for (int i = 0; i < _loggers.Length; i++)
+        {
+            _loggers[i].Log(logLevel, eventId, state, exception, formatter);
+        }
+    }
+
+    private class CompositeDisposable : IDisposable
+    {
+        private readonly IDisposable?[] _disposables;
+
+        public CompositeDisposable(IDisposable?[] disposables) => _disposables = disposables;
+
+        public void Dispose()
+        {
+            foreach (var disposable in _disposables)
+                disposable?.Dispose();
+        }
+    }
 }
